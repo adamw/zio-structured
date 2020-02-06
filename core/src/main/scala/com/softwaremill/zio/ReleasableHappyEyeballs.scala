f33a60147066495f83fbfadb7b8634301a6b6495
adamw <adam@warski.org>
2020-02-06 08:53:13 +0100

    Releasable happy eyeballs with a second release-extra stage

---
+++ core/src/main/scala/com/softwaremill/zio/ReleasableHappyEyeballs.scala
@@ -0,0 +1,27 @@
+package com.softwaremill.zio
+
+import zio.Exit.{Failure, Success}
+import zio._
+import zio.clock.Clock
+import zio.duration._
+
+object ReleasableHappyEyeballs {
+  def apply[R, T](
+      tasks: List[ZIO[R, Throwable, T]],
+      delay: Duration,
+      releaseExtra: T => ZIO[R, Nothing, Unit]
+  ): ZIO[R with Clock, Throwable, T] =
+    for {
+      successful <- Queue.bounded[T](tasks.size)
+      tasks2 = tasks.map {
+        _.onExit {
+          case Success(value) => successful.offer(value)
+          case Failure(_)     => ZIO.unit
+        }
+      }
+      _ <- HappyEyeballs(tasks2, delay)
+      // there has to be at least one, otherwise HE would fail
+      first :: other <- successful.takeAll
+      _ <- ZIO.foreach(other)(releaseExtra)
+    } yield first
+}
--- core/src/main/scala/com/softwaremill/zio/TestSocket.scala
+++ core/src/main/scala/com/softwaremill/zio/TestSocket.scala
@@ -10,24 +10,13 @@ object TestSocket extends App {
   override def run(args: List[String]): ZIO[zio.ZEnv, Nothing, Int] = {
     effectBlocking(InetAddress.getAllByName("debian.org").toList)
       .map { addresses =>
-        addresses.map(a => newSocket(a, 443))
+        addresses.map(a => effectBlocking(new Socket(a, 443)))
       }
-      .flatMap(tasks => HappyEyeballs(tasks, 250.milliseconds))
+      .flatMap(tasks => ReleasableHappyEyeballs(tasks, 250.milliseconds, closeSocket))
       .tap(v => putStrLn(s"Connected: ${v.getInetAddress}"))
       .tapError(error => putStrLn(s"ERROR: $error"))
       .fold(_ => 1, _ => 0)
   }
 
-  def newSocket(address: InetAddress, port: Int): ZIO[Blocking, Throwable, Socket] = {
-    @volatile var socket: Socket = null
-    val closeSocket = effectBlocking(if (socket != null) socket.close())
-      .catchAll(_ => ZIO.unit)
-
-    // the interrupt might have happened when the socket is already established.
-    // In this case, we need to cleanup and close the socket.
-    effectBlocking {
-      socket = new Socket(address, port)
-      socket
-    }.onInterrupt(closeSocket)
-  }
+  def closeSocket(s: Socket): ZIO[Blocking, Nothing, Unit] = effectBlocking(s.close()).catchAll(_ => ZIO.unit)
 }
